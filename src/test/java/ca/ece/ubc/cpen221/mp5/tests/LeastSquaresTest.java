4e14e12b8a5f592c7e18d7f0a23aefbc1a750828
andrewliu5050 <andrewliu5050@gmail.com>
2017-12-01 18:41:30 -0800

    added regression tests

--- src/test/java/ca/ece/ubc/cpen221/mp5/tests/LeastSquaresTest.java
+++ src/test/java/ca/ece/ubc/cpen221/mp5/tests/LeastSquaresTest.java
@@ -2,6 +2,7 @@ package ca.ece.ubc.cpen221.mp5.tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import ca.ece.ubc.cpen221.mp5.*;
@@ -13,32 +14,106 @@ import java.util.function.ToDoubleBiFunction;
 public class LeastSquaresTest {
 
 	@Test
-	public void test1() throws IOException {
+	public void equalityTest1() throws IOException {
+		double delta = 0.0001;
 		YelpDB myDatabase = new YelpDB("data/restaurants.json", "data/reviews.json", "data/users.json");
-	//	System.out.println(myDatabase.getPredictorFunction("hhUQtt5Lksz5aZsvqa4Dbg")); throws an exception
-	//	System.out.println(myDatabase.getPredictorFunction("_n9N41zBLY8uFLyTdynJ1A")); throws an exception
-	//	System.out.println(myDatabase.getPredictorFunction("fL8ujZ89qTyhbjr1Qz5aSg")); does not throw exception
-		//2m6a-NgqiTrIoxwe2YuEag user id should throw exception
 		ToDoubleBiFunction myFunction = myDatabase.getPredictorFunction("fL8ujZ89qTyhbjr1Qz5aSg");
-		double result = myFunction.applyAsDouble(myDatabase, "loBOs5ruFXSNL-ZM29cTrA"); //price is 2
-		double result1 = myFunction.applyAsDouble(myDatabase, "fcdjnsgO8Z5LthXUx3y-lA"); //price is 2
-		double result2 = myFunction.applyAsDouble(myDatabase, "7x0i0seshwLXBVAam16wuw"); //price is 2
 		
-		double result3 = myFunction.applyAsDouble(myDatabase, "1E2MQLWfwpsId185Fs2gWw"); //price is 1
-		double result4 = myFunction.applyAsDouble(myDatabase, "gclB3ED6uk6viWlolSb_uA"); //price is 1
-		double result5 = myFunction.applyAsDouble(myDatabase, "ZMqhKMjtdqVZLw11ja3ANg"); //price is 1
-
+		double result0 = myFunction.applyAsDouble(myDatabase, "1E2MQLWfwpsId185Fs2gWw"); //price is 1
+		double result1 = myFunction.applyAsDouble(myDatabase, "gclB3ED6uk6viWlolSb_uA"); //price is 1
+		double result2 = myFunction.applyAsDouble(myDatabase, "ZMqhKMjtdqVZLw11ja3ANg"); //price is 1
+		assertEquals(result0, result1, delta);
+		assertEquals(result1, result2, delta);
+		assertEquals(result2, result0, delta);
+		
+		double result3 = myFunction.applyAsDouble(myDatabase, "loBOs5ruFXSNL-ZM29cTrA"); //price is 2
+		double result4 = myFunction.applyAsDouble(myDatabase, "fcdjnsgO8Z5LthXUx3y-lA"); //price is 2
+		double result5 = myFunction.applyAsDouble(myDatabase, "7x0i0seshwLXBVAam16wuw"); //price is 2
+		assertEquals(result3, result4, delta);
+		assertEquals(result4, result5, delta);
+		assertEquals(result5, result3, delta);
+		
 		double result6 = myFunction.applyAsDouble(myDatabase, "h_we4E3zofRTf4G0JTEF0A"); //price is 3
 		double result7 = myFunction.applyAsDouble(myDatabase, "bDfcpkyVlSQUvA57HjDTKQ"); //price is 3
 		double result8 = myFunction.applyAsDouble(myDatabase, "g5S6Q9W0IwN5AEK3fNWzbw"); //price is 3
-
+		assertEquals(result6, result7, delta);
+		assertEquals(result7, result8, delta);
+		assertEquals(result8, result6, delta);
+		
 		double result9 = myFunction.applyAsDouble(myDatabase, "HXni0_SFPT1jAoH-Sm78Jg"); //price is 4
 		double result10 = myFunction.applyAsDouble(myDatabase, "6QZR4ToHKlse0yhqpU5ijg"); //price is 4
 		double result11 = myFunction.applyAsDouble(myDatabase, "BiZAzf2SAFCE0gJrX-PGFw"); //price is 4
+		assertEquals(result9, result10, delta);
+		assertEquals(result10, result11, delta);
+		assertEquals(result11, result9, delta);
+	}
+	
+	@Test
+	public void equalityTest2() throws IOException {
+		double delta = 0.0001;
+		YelpDB myDatabase = new YelpDB("data/restaurants.json", "data/reviews.json", "data/users.json");
+		ToDoubleBiFunction myFunction = myDatabase.getPredictorFunction("Rto4xWr5gXA2IbrfyAn-Xg");
+		
+		double result0 = myFunction.applyAsDouble(myDatabase, "1E2MQLWfwpsId185Fs2gWw"); //price is 1
+		double result1 = myFunction.applyAsDouble(myDatabase, "gclB3ED6uk6viWlolSb_uA"); //price is 1
+		double result2 = myFunction.applyAsDouble(myDatabase, "ZMqhKMjtdqVZLw11ja3ANg"); //price is 1
+		assertEquals(result0, result1, delta);
+		assertEquals(result1, result2, delta);
+		assertEquals(result2, result0, delta);
+		
+		double result3 = myFunction.applyAsDouble(myDatabase, "loBOs5ruFXSNL-ZM29cTrA"); //price is 2
+		double result4 = myFunction.applyAsDouble(myDatabase, "fcdjnsgO8Z5LthXUx3y-lA"); //price is 2
+		double result5 = myFunction.applyAsDouble(myDatabase, "7x0i0seshwLXBVAam16wuw"); //price is 2
+		assertEquals(result3, result4, delta);
+		assertEquals(result4, result5, delta);
+		assertEquals(result5, result3, delta);
+		
+		double result6 = myFunction.applyAsDouble(myDatabase, "h_we4E3zofRTf4G0JTEF0A"); //price is 3
+		double result7 = myFunction.applyAsDouble(myDatabase, "bDfcpkyVlSQUvA57HjDTKQ"); //price is 3
+		double result8 = myFunction.applyAsDouble(myDatabase, "g5S6Q9W0IwN5AEK3fNWzbw"); //price is 3
+		assertEquals(result6, result7, delta);
+		assertEquals(result7, result8, delta);
+		assertEquals(result8, result6, delta);
+		
+		double result9 = myFunction.applyAsDouble(myDatabase, "HXni0_SFPT1jAoH-Sm78Jg"); //price is 4
+		double result10 = myFunction.applyAsDouble(myDatabase, "6QZR4ToHKlse0yhqpU5ijg"); //price is 4
+		double result11 = myFunction.applyAsDouble(myDatabase, "BiZAzf2SAFCE0gJrX-PGFw"); //price is 4
+		assertEquals(result9, result10, delta);
+		assertEquals(result10, result11, delta);
+		assertEquals(result11, result9, delta);
+	}
+	
+	@Test
+	public void userThrowsExceptionTest1() throws IOException {
+		double delta = 0.0001;
+		YelpDB myDatabase = new YelpDB("data/restaurants.json", "data/reviews.json", "data/users.json");
 
+		try { 
+			myDatabase.getPredictorFunction("hhUQtt5Lksz5aZsvqa4Dbg"); 
+			fail("should have seen an exception");
+		} catch (IllegalArgumentException e) {
+		}
+	}
+	@Test
+	public void userThrowsExceptionTest2() throws IOException {
+		double delta = 0.0001;
+		YelpDB myDatabase = new YelpDB("data/restaurants.json", "data/reviews.json", "data/users.json");
 
-		System.out.println(result9);
-		System.out.println(result10);
-		System.out.println(result11);
+		try { 
+			myDatabase.getPredictorFunction("_n9N41zBLY8uFLyTdynJ1A"); 
+			fail("should have seen an exception");
+		} catch (IllegalArgumentException e) {
+		}
+	}
+	@Test
+	public void userThrowsExceptionTest3() throws IOException {
+		double delta = 0.0001;
+		YelpDB myDatabase = new YelpDB("data/restaurants.json", "data/reviews.json", "data/users.json");
+
+		try { 
+			myDatabase.getPredictorFunction("2m6a-NgqiTrIoxwe2YuEag"); 
+			fail("should have seen an exception");
+		} catch (IllegalArgumentException e) {
+		}
 	}
 }
